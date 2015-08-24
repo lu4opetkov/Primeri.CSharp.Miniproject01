a6a0f6449a09607ba884eec2ee21b1b5db7994b2
Лъчезар Петков <lu4i@abv.bg>
2015-08-24 22:20:47 +0300

    Модул 06, Видео 04 - Библиотека за основното меню

--- MP01/UserInput/UserInput.cs
+++ MP01/UserInput/UserInput.cs
@@ -7,6 +7,24 @@ namespace UserInput
 		public UInput ()
 		{
 		}
+		public void getUserCommands ()
+		{
+			string command = "";
+			do {
+				// Взимане на команда
+				Console.Write("$ ");
+				command = Console.ReadLine();
+
+				// Проверка за налични команди
+
+				if (command.ToLower().Contains("команда1"))  Console.WriteLine("Стартирана е команда 1");
+				if (command.ToLower().Contains("команда2"))  Console.WriteLine("Стартирана е команда 2");
+				if (command.ToLower().Contains("команда3"))  Console.WriteLine("Стартирана е команда 3");	
+
+			} while (command.ToLower() != "изход");
+		}
+
+
 		public void justTesting ()
 		{
 			Console.WriteLine ("Стартиране на UserInput.UInput.justTesting()");
