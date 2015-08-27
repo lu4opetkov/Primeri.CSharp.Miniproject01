254ded55ce6d6bfa426a0d1d1787f3a59039f998
Лъчезар Петков <lu4i@abv.bg>
2015-08-27 22:30:18 +0300

    Модул 06 - Видео 08 - Форматиране на първата формула

--- MP01/Calculations/Formula01.cs
+++ MP01/Calculations/Formula01.cs
@@ -4,6 +4,8 @@ namespace Calculations
 {
 	public class Formula01
 	{
+		//Библиотеки
+		private Colors.ForCLI _c = new Colors.ForCLI();
 		public Formula01 () // Формула за обикновена строителна яма
 		{
 		}
@@ -12,20 +14,23 @@ namespace Calculations
 		{
 			try {
 				string[] param = _userInput.Split (' ');
-				if (param.Length > 1 && _userInput.Contains ("-п")) {
+				if (param.Length > 1 && _userInput.Contains ("-п")) 
+				{
 					//Помощ за командата
+					help();
 				}
 				if (param.Length > 1 && !_userInput.Contains ("-п")) {
 					//Изчисления
 					double _result = 0;
 					if (runCalculations(param, out _result))
 					{
-						Console.Write("Обема на строителната яма е: " + _result.ToString("N2") + " м3\n\n" );
-
+						_c.Default(); Console.Write("Обема на строителната яма е: " );
+						_c.Result(); Console.Write (_result.ToString("N2") );
+						_c.Default(); Console.WriteLine(" м3\n\n");
 					}
 					else
 					{
-						Console.WriteLine("Има грешно въведени параметри. Може да проверите синтаксиса с параметъра -п ");
+						_c.Default(); Console.WriteLine("Има грешно въведени параметри. Може да проверите синтаксиса с параметъра -п ");
 					}
 				}
 
@@ -57,16 +62,25 @@ namespace Calculations
 			return false;
 			
 		}
-					
-
-			
-		}
-
-		//Изчисление и изписване
-
-
 		//Помощ за командата
+		private void help ()
+		{
+			_c.Default(); Console.Write("[яма]");
+			_c.Result(); Console.WriteLine(" - команда за пресмятане на строителна яма");
 
+			_c.Command(); Console.Write(" параметри: ");
+			_c.Result(); Console.WriteLine(" a1 b1 a2 b2 h \n");
+
+			_c.Command(); Console.Write(" a1 и b1 ");
+			_c.Result(); Console.WriteLine(" - ширина и дължина на горната страна на изкопа");
+
+			_c.Command(); Console.Write(" a2 и b2 ");
+			_c.Result(); Console.WriteLine(" - ширина и дължина на долната страна на изкопа");
+
+			_c.Command(); Console.Write(" h ");
+			_c.Result(); Console.WriteLine(" - дълбочина на изкопа \n");
+		}
 	}
+}
 
 
